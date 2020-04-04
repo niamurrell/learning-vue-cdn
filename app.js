88f50b0bc2bba29f2e47702a556d305e72483057
niamurrell <niamurrell@users.noreply.github.com>
2020-04-04 09:51:14 +0100

    Vue attributes help with data binding

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ new Vue({
   el: '#vue-app',
   data: {
     name: 'Nonna',
-    job: 'Baller'
+    job: 'Baller',
+    website: 'https://www.google.com',
+    websiteElement: '<a href="https://www.google.com">Google</a>',
   },
   methods: {
     greet: function () {
