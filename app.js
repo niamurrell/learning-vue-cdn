f2a0c6181d6cede9057174b75f74dfffa9dbf62f
niamurrell <niamurrell@users.noreply.github.com>
2020-04-04 12:12:41 +0100

    Event handling basics

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ new Vue({
     job: 'Baller',
     website: 'https://www.google.com',
     websiteElement: '<a href="https://www.google.com">Google</a>',
+    bottleCount: 11,
   },
   methods: {
-    greet: function () {
-      return `Good morning ${this.name}!`
-    }
+    greet: function () { return `Good morning ${this.name}!` },
+    addTwo: function () { this.bottleCount += 2 },
+    minusTwo: function () { this.bottleCount -= 2 },
   }
 });
