38a4aa51936073a666697937b55d509edadfefe9
niamurrell <niamurrell@users.noreply.github.com>
2020-04-04 14:05:36 +0100

    Update CSS with computed classes

--- app.js
+++ app.js
@@ -10,16 +10,21 @@ new Vue({
     a: 0,
     b: 0,
     favCountry: 'Iceland',
+    availableDemo: false,
+    available: false,
+    nearby: false,
   },
   methods: {
     greet: function () { return `Good morning ${this.name}!` },
     addTwo: function () { console.log('addTwo') && (this.bottleCount += 2) },
     minusTwo: function () { console.log('minusTwo') && (this.bottleCount -= 2) },
-    addToA: function () { console.log('addToA method'); return this.kombuchaCount + this.a },
-    addToB: function () { console.log('addToB method'); return this.kombuchaCount + this.b },
+    // the console logs are commented to stop them running on every button click:
+    addToA: function () { /* console.log('addToA method'); */ return this.kombuchaCount + this.a },
+    addToB: function () { /* console.log('addToB method'); */ return this.kombuchaCount + this.b },
   },
   computed: {
-    addToAComputed: function () { console.log('addToA computed function'); return this.kombuchaCount + this.a },
-    addToBComputed: function () { console.log('addToB computed function'); return this.kombuchaCount + this.b },
+    addToAComputed: function () { /* console.log('addToA computed function'); */ return this.kombuchaCount + this.a },
+    addToBComputed: function () { /* console.log('addToB computed function'); */ return this.kombuchaCount + this.b },
+    computedClasses: function () { return { available: this.available, nearby: this.nearby, } },
   },
 });
